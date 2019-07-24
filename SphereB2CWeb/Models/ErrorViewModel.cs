defe54308f43799ff5da1e855ade679fc7545a4a
Fred Murphy <fred27murphy@gmail.com>
2019-07-24 20:50:46 +0100

    Basic MVC website with Azure B2C authentication

---
+++ SphereB2CWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SphereB2CWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
